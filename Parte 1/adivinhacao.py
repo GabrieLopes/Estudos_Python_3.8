3b6b9e609b25e75fc02df20f211206df37c99364
Gabriel Lopes <gabriel_lopes1910@outlook.com>
2020-04-15 15:18:52 -0300

    Aula 3.1 - A condição elif

--- Parte 1/adivinhacao.py
+++ Parte 1/adivinhacao.py
@@ -10,9 +10,16 @@ print("Você digitou ", chute_str)
 
 chute = int(chute_str)
 
-if numero_secreto == chute:
+acertou = numero_secreto == chute
+maior   = chute > numero_secreto
+menor   = chute < numero_secreto
+
+if acertou:
     print("Você acertou")
 else:
-    print("Você errou")
+    if maior:
+        print("Você errou! O seu chute foi maior do que o numero secreto.")
+    elif menor:
+        print("Você errou! O seu chute foi menor do que o numero secreto.")
 
 print("Fim do jogo")
