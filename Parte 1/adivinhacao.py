c1463ad4cce5d4231321d8cb47ba52cb664c76bb
Gabriel Lopes <gabriel_lopes1910@outlook.com>
2020-04-16 10:08:31 -0300

    Aula 5.2 - Encerrando a iteração e o loop

--- Parte 1/adivinhacao.py
+++ Parte 1/adivinhacao.py
@@ -9,9 +9,13 @@ total_de_tentativas = 3
 for rodada in range(1, total_de_tentativas + 1):
     print("Tentiva {} de {}".format(rodada, total_de_tentativas))
     
-    chute_str = input("Digite seu número: ")
+    chute_str = input("Digite um número entre 1 e 100: ")
     print("Você digitou ", chute_str)
     chute = int(chute_str)
+
+    if (chute < 1 or chute > 100):
+        print("Você deve digitar um numero entre 1 e 100!")
+        continue
 
     acertou = numero_secreto == chute
     maior   = chute > numero_secreto
@@ -19,6 +23,7 @@ for rodada in range(1, total_de_tentativas + 1):
 
     if (acertou):
         print("Você acertou")
+        break
     else:
         if (maior):
             print("Você errou! O seu chute foi maior do que o numero secreto.")
