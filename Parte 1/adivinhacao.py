5cb3e395915e4df3c6ff8d065a81b52776141af1
Gabriel Lopes <gabriel_lopes1910@outlook.com>
2020-04-16 09:56:46 -0300

    Aula 5.1 - O laço com for

--- Parte 1/adivinhacao.py
+++ Parte 1/adivinhacao.py
@@ -4,10 +4,11 @@ print("*********************************")
 
 numero_secreto = 42
 total_de_tentativas = 3
-rodada = 1
 
-while(rodada <= total_de_tentativas):
+
+for rodada in range(1, total_de_tentativas + 1):
     print("Tentiva {} de {}".format(rodada, total_de_tentativas))
+    
     chute_str = input("Digite seu número: ")
     print("Você digitou ", chute_str)
     chute = int(chute_str)
@@ -23,7 +24,5 @@ while(rodada <= total_de_tentativas):
             print("Você errou! O seu chute foi maior do que o numero secreto.")
         elif (menor):
             print("Você errou! O seu chute foi menor do que o numero secreto.")
-
-    rodada = rodada + 1
 
 print("Fim do jogo")
