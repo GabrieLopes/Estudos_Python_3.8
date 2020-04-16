5ab20b30a68ec164c4de3ae3cb70f8298761aa61
Gabriel Lopes <gabriel_lopes1910@outlook.com>
2020-04-16 13:17:59 -0300

    Aula 8.2 - Criando funções para os nossos jogos

--- Parte 1/forca.py
+++ Parte 1/forca.py
@@ -1,5 +1,7 @@
-print("*********************************")
-print("Bem vindo ao jogo de Forca!")
-print("*********************************")
 
-print("Fim do jogo")
+def jogar():
+    print("*********************************")
+    print("***Bem vindo ao jogo de Forca!***")
+    print("*********************************")
+
+    print("Fim do jogo")
--- Parte 1/jogos.py
+++ Parte 1/jogos.py
@@ -10,5 +10,7 @@ print("(1) Forca (2) Adivinhação")
 jogo = int(input("Qual jogo?"))
 if(jogo == 1):
     print("Jogando forca")
+    forca.jogar()
 elif(jogo == 2):
     print("Jogando adivinhação")
+    adivinhacao.jogar()
